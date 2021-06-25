85fe8b3c1d36348020af506c8ffde1c9aab3cf51
Christine Chen <chenchr@umich.edu>
2021-06-25 16:22:37 -0400

    Footer

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,4 @@ export { default as Layout } from './layout';
 export { default as Head } from './head';
 /* Sections */
 export { default as Hero } from './sections/hero';
+export { default as Footer } from './sections/footer';
---
+++ src/components/sections/footer.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const DashedLine = styled.div`
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(to right, transparent 50%, var(--background) 50%),
+    var(--gradient);
+  background-size: 12px 2px, 100% 1px;
+`;
+
+const StyledFooter = styled.footer`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.25rem;
+  font-size: var(--fz-sm);
+`;
+
+const getYear = () => new Date().getFullYear();
+
+/* TODO: Add link */
+const Footer = (): JSX.Element => {
+  return (
+    <StyledFooter>
+      <span>
+        ⚙ Designed & built by <mark>Christine Chen</mark> {getYear()}
+      </span>
+      <DashedLine />
+    </StyledFooter>
+  );
+};
+
+export default Footer as React.FunctionComponent;
