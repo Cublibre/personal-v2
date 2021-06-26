94261b1ea1c2d33983bb566e6da3b73a8ebb2ee8
Christine Chen <chenchr@umich.edu>
2021-06-25 23:54:22 -0400

    Experience section layout and style

--- src/components/sections/exp.tsx
+++ src/components/sections/exp.tsx
@@ -1,6 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useStaticQuery, graphql } from 'gatsby';
+
+const ExpContainer = styled.div`
+  display: flex;
+  --pad: 16px;
+`;
+
+const ExpLeft = styled.div`
+  text-align: right;
+  padding: var(--pad) var(--pad) 0 0;
+  min-width: 4rem;
+  height: 100%;
+`;
+
+const ExpRight = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  padding: var(--pad);
+
+  ul {
+    margin-top: 4px;
+    list-style: none;
+  }
+  li:before {
+    content: '» ';
+    color: var(--primary);
+  }
+`;
+
+const VerticalLine = styled.div`
+  border-left: 1px solid var(--divide);
+  margin-top: var(--pad);
+`;
+
+const StyledDate = styled.span`
+  font-family: var(--font-mono);
+  font-weight: var(--fw-medium);
+  font-size: var(--fz-sm);
+  color: var(--highlight);
+`;
+
 interface IExpNode {
   html: string;
   frontmatter: {
@@ -40,12 +81,17 @@ const Experience = (): JSX.Element => {
     const { frontmatter, html } = node;
     const { title, range, place } = frontmatter;
     return (
-      <div key={`exp-inner-${title}`}>
-        <h4>{place}</h4>
-        <h3>{title}</h3>
-        <p>{range}</p>
-        <div dangerouslySetInnerHTML={{ __html: html }}></div>
-      </div>
+      <ExpContainer key={`exp-inner-${title}`}>
+        <ExpLeft>
+          <h4>{place}</h4>
+        </ExpLeft>
+        <VerticalLine />
+        <ExpRight>
+          <h3>{title}</h3>
+          <StyledDate>{range}</StyledDate>
+          <div dangerouslySetInnerHTML={{ __html: html }}></div>
+        </ExpRight>
+      </ExpContainer>
     );
   };
 
