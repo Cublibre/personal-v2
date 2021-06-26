680ac8f2996dad5aa0db62879a273b8146ff0924
Christine Chen <chenchr@umich.edu>
2021-06-26 14:08:58 -0400

    Set width and margin of layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Normalize } from 'styled-normalize';
 import { Head } from '@components';
 import { GlobalStyle } from '@styles';
+
+const StyledLayout = styled.div`
+  max-width: 750px;
+  margin-left: 25%;
+`;
 
 type LayoutProps = {
   children?: React.ReactNode;
@@ -9,12 +15,12 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div id="" role="document">
+    <StyledLayout id="" role="document">
       <Normalize />
       <GlobalStyle />
       <Head />
       <main>{children}</main>
-    </div>
+    </StyledLayout>
   );
 };
 
