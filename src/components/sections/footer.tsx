fef1f3c29585bd9fad8634bf9860e44e752b0a31
Christine Chen <chenchr@umich.edu>
2021-06-26 14:32:33 -0400

    Adjust footer

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const DashedLine = styled.div`
   width: 100%;
-  height: 2px;
+  height: 1px;
   background: linear-gradient(to right, transparent 50%, var(--background) 50%),
     var(--gradient);
   background-size: 12px 2px, 100% 1px;
@@ -13,7 +13,8 @@ const StyledFooter = styled.footer`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 0.25rem;
+  gap: 0.5rem;
+  padding-bottom: 0.5rem;
   font-size: var(--fz-sm);
 `;
 
@@ -23,10 +24,10 @@ const getYear = () => new Date().getFullYear();
 const Footer = () => {
   return (
     <StyledFooter>
+      <DashedLine />
       <span>
         ⚙ Designed & built by <mark>Christine Chen</mark> {getYear()}
       </span>
-      <DashedLine />
     </StyledFooter>
   );
 };
